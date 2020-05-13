7973a88d268bb97fe93093c295d33a2a81114597
illic <rubanovas2005@gmail.com>
2020-05-13 16:41:58 +0000

    Change

--- func.h
+++ func.h
@@ -1,8 +1,4 @@
 #include <stdio.h>
 
-#define MAX_PATH 260
-#define LENGTH 1000
-
-
-
-void scan_paths(char *str, char *delim);
+void scan_paths(char* str, char* delim);
+char* refresh(char* path, char* delim);
--- main.c
+++ main.c
@@ -1,100 +1,20 @@
+#include "func.h"
+#include "math.h"
+#include "strings.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "func.h"
-#include "strings.h"
-#include "math.h"
 
-const char * protocols[2] = {
-"http:",
-"smb:"
-};
+int main(void)
+{
+    char delim[0];
+    char* path = malloc(1000);
+    char* out = malloc(1000);
 
-const char * domains[3] = {
-"org",
-"com",
-"ru"
-};
+    scan_paths(path, delim);
+    out = refresh(path, delim);
 
+    printf("%s\n", out);
+    free(out);
 
-int replace_sym(char *str, char sym, char replace){
-  if(!str)
-    return -1;
-  int count = 0;
-  for(int i = 0;i<slen(str);i++){
-    if(str[i]==sym){
-      str[i]=replace;
-      count++;
-    }
-  }
-  return count;
-}
-
-int check(char path[]){
-  char* tokens[10];
-  int tokensCount = stok(path, '/', tokens);
-  
-
-  return 0; 
-}
-
-
-char * refresh(char * path, char * delim){
-  if(!path)
-    return NULL;
-  char* paths[10];
-  int pathsCount;
-  pathsCount = stok(path,  delim[0], paths);
-  char* tokens[10];
-  char* new_paths[10];
-  int tokensCount;
-  int npathCount=0;
-  for(int i=0 ;i<pathsCount;i++){
-    tokensCount = stok(paths[i], '/', tokens);
-
-    printf(" %s ",tokens[2]);
-    char * buf=tokens[2];
-    printf(" %s\n",buf);
-
-    if(!scmp(tokens[0],protocols[0]) || !scmp(tokens[0],protocols[1])){
-
-    
-
-
-    char * temp=malloc(MAX_PATH);
-    temp=scat(temp,"\\");
-    for(int j=2 ;j<tokensCount;j++){
-      //printf(" %s",tokens[j]);
-      temp=scat(temp,"\\");
-      temp=scat(temp,tokens[j]);
-    }
-    new_paths[npathCount]=temp;
-    npathCount++;
-    }
-  }
-
-  char * res=malloc(LENGTH);
-  res=scat(res,new_paths[0]);
-  for(int i=1 ;i<npathCount;i++){ 
-    res=scat(res,"+"); 
-    res=scat(res,new_paths[i]);  
-  }
-  return res;
-}
-
-
-int main(void) {
-  char delim[0];
-	char * path=malloc(LENGTH);
-
-	scan_paths(path, delim);
-  path = refresh(path, delim);
-
-  printf("%s\n",path);
-  free(path);
-
-
-
-
-  
-  return 0;
+    return 0;
 }
